8996ee99f31b46084263ce6cd84e8af129be070a
Marat <gdtawr@gmail.com>
2025-04-23 23:00:03 +0300

    closeness_centrality

--- practicum_3/homework/centrality_measures.py
+++ practicum_3/homework/centrality_measures.py
@@ -3,6 +3,7 @@ from itertools import combinations
 
 import numpy as np
 import networkx as nx
+from heapq import heappop, heappush
 
 from src.plotting.graphs import plot_graph, plot_network_via_plotly
 from src.common import AnyNxGraph 
@@ -14,12 +15,12 @@ class CentralityMeasure(Protocol):
 
 
 def closeness_centrality(G: AnyNxGraph) -> dict[Any, float]:
-
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    #########################
-
-    pass
+    closeness_centralities = {} 
+    g_range = range(len(G))
+    for n in g_range:
+        distances = nx.shortest_path_length(G, source=n)
+        closeness_centralities[n] = 1 / sum(distances[i] for i in g_range)
+    return closeness_centralities
 
 
 def betweenness_centrality(G: AnyNxGraph) -> dict[Any, float]: 
@@ -50,8 +51,9 @@ def plot_centrality_measure(G: AnyNxGraph, measure: CentralityMeasure) -> None:
 
 if __name__ == "__main__":
     G = nx.karate_club_graph()
-    
-    plot_centrality_measure(G, closeness_centrality)
-    plot_centrality_measure(G, betweenness_centrality)
-    plot_centrality_measure(G, eigenvector_centrality)
+    print(nx.closeness_centrality(G))
+    print(closeness_centrality(G))
+    #plot_centrality_measure(G, closeness_centrality)
+    #plot_centrality_measure(G, betweenness_centrality)
+    #plot_centrality_measure(G, eigenvector_centrality)
 
