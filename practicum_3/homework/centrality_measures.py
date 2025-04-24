6710f2e9f1dff2dc455bdc16c472d03fb171c21c
Marat <gdtawr@gmail.com>
2025-04-24 20:26:24 +0300

    final fix for closeness centrality

--- practicum_3/homework/centrality_measures.py
+++ practicum_3/homework/centrality_measures.py
@@ -13,14 +13,14 @@ class CentralityMeasure(Protocol):
     def __call__(self, G: AnyNxGraph) -> dict[Any, float]:
         ...
 
-def closeness_centrality(G: AnyNxGraph) -> dict[Any, float]:
-    closeness_centralities = {} 
-    g_range = range(len(G))
-    for n in g_range:
-        distances = nx.shortest_path_length(G, source=n)
-        closeness_centralities[n] = 1 / sum(distances[i] for i in g_range)
-    return closeness_centralities
-
+def closeness_centrality(G: nx.Graph) -> dict[Any, float]:
+    centrality = {}
+    nodes = list(G.nodes())
+    n = len(nodes)
+    for node in nodes:
+        total_distance = sum(nx.shortest_path_length(G, source=node).values())
+        centrality[node] = (n - 1) / total_distance
+    return centrality
 
 def betweenness_centrality(G: AnyNxGraph) -> dict[Any, float]: 
 
@@ -50,8 +50,8 @@ def plot_centrality_measure(G: AnyNxGraph, measure: CentralityMeasure) -> None:
 
 if __name__ == "__main__":
     G = nx.karate_club_graph()
-    print(nx.closeness_centrality(G))
     print(closeness_centrality(G))
+    print(nx.closeness_centrality(G))
     #plot_centrality_measure(G, closeness_centrality)
     #plot_centrality_measure(G, betweenness_centrality)
     #plot_centrality_measure(G, eigenvector_centrality)
